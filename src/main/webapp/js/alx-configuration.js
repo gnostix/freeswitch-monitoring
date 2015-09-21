cfc95569f5e8250d070272e2e2ab7012124c78c2
Alex Pappas <apappas@gnostix.gr>
2015-09-21 19:03:11 +0300

    conn add client js

--- src/main/webapp/js/alx-configuration.js
+++ src/main/webapp/js/alx-configuration.js
@@ -4,7 +4,6 @@ $(function () {
 
     $(document).ready(function () {
 
-
         // process the form
         $('form#eslConnForm').submit(function (event) {
               // get the form data
@@ -28,16 +27,36 @@ $(function () {
                 // using the done promise callback
                 .done(function (data) {
                     // log data to the console so we can see
-                    console.log("on success " + data);
+                    console.log("on success " + JSON.stringify(data));
                     // here we will handle errors and validation messages
+                    if(data.status===200){
+                        //this removes a class name
+                        //$(".form-horizontal").remove();
+                        //this replace text in a  class
+                        $( ".inputRes" ).replaceWith( data.message);
+                        //this changes the class
+                        $("#message").toggleClass('alert-info alert-success');
+                        $('#table > tbody:last').append('<tr><td>'+formData.ip+'</td><td>'+formData.port+'</td><td><button type="button" class="btn btn-danger remove" onclick ="delete_user($(this),\''+formData.ip+'\')">Delete</button></td></tr>');
+
+                    } else if(data.status===400){
+                        //this removes a class name
+                        //$(".form-horizontal").remove();
+                        //this replace text in a  class
+                        $( ".inputRes" ).replaceWith( data.message);
+                        //this changes the class
+                       // $("#message").toggleClass('alert-info alert-success');
+                       // $('#table > tbody:last').append('<tr><td>'+formData.ip+'</td><td>'+formData.port+'</td><td><button type="button" class="btn btn-danger remove" onclick ="delete_user($(this),\''+formData.ip+'\')">Delete</button></td></tr>');
+
+                    }
+
                 })
                 // using the fail promise callback
                 .fail(function(data) {
                     // show any errors
                     // best to remove for production
                     console.log("on fail " + data);
-                    $(".form-horizontal").remove();
-                    $( ".inputRes" ).replaceWith( data );
+                    $( ".inputRes" ).replaceWith( data.message );
+                    $("#message").toggleClass('alert-info alert-success');
                 });
 
             // stop the form from submitting the normal way and refreshing the page
@@ -48,3 +67,31 @@ $(function () {
 
 
 });
+
+function delete_user(row,ip)
+{
+    //alert(ip);
+    $.ajax({
+        type: 'DELETE', // define the type of HTTP verb we want to use (POST for our form)
+        url: 'http://fs-moni.cloudapp.net:8080/configuration/fs-node/conn-data', // the url where we want to POST
+        data: JSON.stringify(ip), // our data object
+        dataType: 'json', // what type of data do we expect back from the server
+        contentType: "application/json",
+        encode: true
+    })
+        // using the done promise callback
+        .done(function (data) {
+            // log data to the console so we can see
+            console.log("on success delete " + JSON.stringify(data));
+            row.closest('tr').remove();
+        })
+        // using the fail promise callback
+        .fail(function(data) {
+            // show any errors
+            // best to remove for production
+            console.log("on fail " + data);
+        });
+
+
+
+}
