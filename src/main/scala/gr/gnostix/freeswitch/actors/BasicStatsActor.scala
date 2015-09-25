445358c954f0a3446115f8ceda17ff6ec1516779
Alex Pappas <apappas@gnostix.gr>
2015-09-25 21:45:04 +0300

    typo

--- src/main/scala/gr/gnostix/freeswitch/actors/BasicStatsActor.scala
+++ src/main/scala/gr/gnostix/freeswitch/actors/BasicStatsActor.scala
@@ -75,11 +75,12 @@ class BasicStatsActor(callRouterActor: ActorRef, completedCallsActor: ActorRef, 
               log info s"------> response from BasicStatsTick failed calls ${r2.failedCalls - totalOfLastValueFailedCalls} - completed calls ${r3.size}"
               val r3Sorted = r3.sortWith { (leftE, rightE) => leftE.callerChannelHangupTime.before(rightE.callerChannelHangupTime) }
               val asr = r3Sorted.size.toDouble / (r3Sorted.size + (r2.failedCalls - totalOfLastValueFailedCalls)) * 100
+                log info s" ------> acd r3Sorted sum ${r3Sorted.map(x => x.acd).sum / r3Sorted.size} and size ${r3Sorted.size}"
               val acd = BigDecimal((r3Sorted.map(x => x.acd).sum / r3Sorted.size) / 60.toDouble).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble //seconds to minutes
               val rtpQ = r3Sorted.map(x => x.rtpQuality).sum / r3Sorted.size.toDouble
-              lastBasicStatsTickTime = r3Sorted.head.callerChannelHangupTime
+              //lastBasicStatsTickTime = r3Sorted.head.callerChannelHangupTime
 
-              (BasicStatsTimeSeries(BASIC_STATS, lastBasicStatsTickTime, r1.calls, r2.failedCalls - totalOfLastValueFailedCalls, acd, asr, rtpQ),r2.failedCalls)
+              (BasicStatsTimeSeries(BASIC_STATS, new Timestamp(System.currentTimeMillis), r1.calls, r2.failedCalls - totalOfLastValueFailedCalls, acd, asr, rtpQ),r2.failedCalls)
             }
 
           }
